b20e6bcb6447712c6a535023f4fd1afd7e07bf51
Ant <Ant@853fb775-85e9-0310-89d4-f5ae88729b16>
2005-05-16 03:44:37 +0000

    Define gdkglext structs
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@62 853fb775-85e9-0310-89d4-f5ae88729b16

--- dyndui/src/lib/gdkglext.d
+++ dyndui/src/lib/gdkglext.d
@@ -11,6 +11,16 @@ private
 private Linker gdkgl_Linker;
 private Linker gtkgl_Linker;
 
+// quick add definitions for GL Extensions
+
+struct GdkGLConfig;
+struct GdkGLConfigMode;
+struct GdkGLContext;
+struct GdkGLDrawable;
+struct GdkGLPixmap;
+struct GdkGLWindow;
+struct VisualID;
+
 static this()
 {
 	gdkgl_Linker = new Linker( libPath ~ importLibs[LIBRARY.GDK_GLEXT] );
@@ -55,13 +65,13 @@ extern(C)
 			gdk_gl_config_new;
 	GdkGLConfig*		function(GdkScreen*screen,int*attrib_list)
 			gdk_gl_config_new_for_screen;
-	GdkGLConfig*		function(GdkGLConfigModemode)
+	GdkGLConfig*		function(GdkGLConfigMode mode)
 			gdk_gl_config_new_by_mode;
-	GdkGLConfig*		function(GdkScreen*screen,GdkGLConfigModemode)
+	GdkGLConfig*		function(GdkScreen*screen,GdkGLConfigMode mode)
 			gdk_gl_config_new_by_mode_for_screen;
 	GdkScreen*		function(GdkGLConfig*glconfig)
 			gdk_gl_config_get_screen;
-	gboolean		function(GdkGLConfig*glconfig,intattribute,int*value)
+	gboolean		function(GdkGLConfig*glconfig,int attribute,int*value)
 			gdk_gl_config_get_attrib;
 	GdkColormap*		function(GdkGLConfig*glconfig)
 			gdk_gl_config_get_colormap;
@@ -89,11 +99,11 @@ extern(C)
 			gdk_gl_config_has_stencil_buffer;
 	gboolean		function(GdkGLConfig*glconfig)
 			gdk_gl_config_has_accum_buffer;
-	GdkGLContext*		function(GdkGLDrawable*gldrawable,GdkGLContext*share_list,gbooleandirect,intrender_type)
+	GdkGLContext*		function(GdkGLDrawable*gldrawable,GdkGLContext*share_list,gboolean direct,int render_type)
 			gdk_gl_context_new;
 	void		function(GdkGLContext*glcontext)
 			gdk_gl_context_destroy;
-	gboolean		function(GdkGLContext*glcontext,GdkGLContext*src,ulongmask)
+	gboolean		function(GdkGLContext*glcontext,GdkGLContext*src,ulong mask)
 			gdk_gl_context_copy;
 	GdkGLDrawable*		function(GdkGLContext*glcontext)
 			gdk_gl_context_get_gl_drawable;
@@ -107,23 +117,23 @@ extern(C)
 			gdk_gl_context_get_render_type;
 	GdkGLContext*		function()
 			gdk_gl_context_get_current;
-	void		function(gbooleansolid,doublesize)
+	void		function(gboolean solid,double size)
 			gdk_gl_draw_cube;
-	void		function(gbooleansolid,doubleradius,intslices,intstacks)
+	void		function(gboolean solid,double radius,int slices,int stacks)
 			gdk_gl_draw_sphere;
-	void		function(gbooleansolid,doublebase,doubleheight,intslices,intstacks)
+	void		function(gboolean solid,double base,double height,int slices,int stacks)
 			gdk_gl_draw_cone;
-	void		function(gbooleansolid,doubleinner_radius,doubleouter_radius,intnsides,intrings)
+	void		function(gboolean solid,double inner_radius,double outer_radius,int nsides,int rings)
 			gdk_gl_draw_torus;
-	void		function(gbooleansolid)
+	void		function(gboolean solid)
 			gdk_gl_draw_tetrahedron;
-	void		function(gbooleansolid)
+	void		function(gboolean solid)
 			gdk_gl_draw_octahedron;
-	void		function(gbooleansolid)
+	void		function(gboolean solid)
 			gdk_gl_draw_dodecahedron;
-	void		function(gbooleansolid)
+	void		function(gboolean solid)
 			gdk_gl_draw_icosahedron;
-	void		function(gbooleansolid,doublescale)
+	void		function(gboolean solid,double scale)
 			gdk_gl_draw_teapot;
 	gboolean		function(GdkGLDrawable*gldrawable,GdkGLContext*glcontext)
 			gdk_gl_drawable_make_current;
@@ -145,9 +155,9 @@ extern(C)
 			gdk_gl_drawable_get_size;
 	GdkGLDrawable*		function()
 			gdk_gl_drawable_get_current;
-	PangoFont*		function(PangoFontDescription*font_desc,intfirst,intcount,intlist_base)
+	PangoFont*		function(PangoFontDescription*font_desc,int first,int count,int list_base)
 			gdk_gl_font_use_pango_font;
-	PangoFont*		function(GdkDisplay*display,PangoFontDescription*font_desc,intfirst,intcount,intlist_base)
+	PangoFont*		function(GdkDisplay*display,PangoFontDescription*font_desc,int first,int count,int list_base)
 			gdk_gl_font_use_pango_font_for_display;
 	GdkGLPixmap*		function(GdkGLConfig*glconfig,GdkPixmap*pixmap,int*attrib_list)
 			gdk_gl_pixmap_new;
@@ -182,9 +192,9 @@ version(linux)
 {
 	gboolean		function(GdkGLConfig*glconfig,char*extension)
 			gdk_x11_gl_query_glx_extension;
-	GdkGLConfig*		function(VisualIDxvisualid)
+	GdkGLConfig*		function(VisualID xvisualid)
 			gdk_x11_gl_config_new_from_visualid;
-	GdkGLConfig*		function(GdkScreen*screen,VisualIDxvisualid)
+	GdkGLConfig*		function(GdkScreen*screen,VisualID xvisualid)
 			gdk_x11_gl_config_new_from_visualid_for_screen;
 	int		function(GdkGLConfig*glconfig)
 			gdk_x11_gl_config_get_screen_number;
@@ -194,13 +204,13 @@ version(linux)
 
 // gtkglext functions
 
-	gboolean		function(GtkWidget*widget,GdkGLConfig*glconfig,GdkGLContext*share_list,gbooleandirect,intrender_type)
+	gboolean		function(GtkWidget*widget,GdkGLConfig*glconfig,GdkGLContext*share_list,gboolean direct,int render_type)
 			gtk_widget_set_gl_capability;
 	gboolean		function(GtkWidget*widget)
 			gtk_widget_is_gl_capable;
 	GdkGLConfig*		function(GtkWidget*widget)
 			gtk_widget_get_gl_config;
-	GdkGLContext*		function(GtkWidget*widget,GdkGLContext*share_list,gbooleandirect,intrender_type)
+	GdkGLContext*		function(GtkWidget*widget,GdkGLContext*share_list,gboolean direct,int render_type)
 			gtk_widget_create_gl_context;
 	GdkGLContext*		function(GtkWidget*widget)
 			gtk_widget_get_gl_context;
