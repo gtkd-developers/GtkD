cbbe6864b331c8331c270676a7480732c60508d6
Alan Knowles <Alan Knowles@853fb775-85e9-0310-89d4-f5ae88729b16>
2006-10-28 02:51:04 +0000

    oops use correct format for setting font - Im supprised that compiled before..?? on another box
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@171 853fb775-85e9-0310-89d4-f5ae88729b16

--- src/gtk/Widget.d
+++ src/gtk/Widget.d
@@ -250,7 +250,9 @@ public class Widget : ObjectGtk
 	public void modifyFont(char[] family, int size)
 	{
 		if ( size < 0 ) size = -size;	// hack to workaround leds bug - TO BE REMOVED
-		modifyFont(PangoFontDescription.fromString(family ~ " " ~ string));
+		modifyFont(new PgFontDescription(
+			PgFontDescription.fromString(
+			family ~ " " ~ std.string.toString(size))));
 
 	}
 	
