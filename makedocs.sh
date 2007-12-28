81c14812c63ffd053356e34c686c660e5b8b4094
Pse <Pse@853fb775-85e9-0310-89d4-f5ae88729b16>
2007-12-28 22:52:29 +0000

    Update makedocs.sh to look into cairo, glade and gdkpixbuf subdirs.
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@336 853fb775-85e9-0310-89d4-f5ae88729b16

--- makedocs.sh
+++ makedocs.sh
@@ -4,8 +4,8 @@ grep -h -e "^module" srcgl/* -r | sort -u | sed 's/;//' | sed 's/\r//' |  sed 's
 
 dmd -o- -D -Dddocs  modules.ddoc docs/candy.ddoc \
 	src/gtk/*  src/gtkc/* src/glib/* src/gdk/* \
-	src/gobject/* src/gthread/* src/atk/* \
-	src/pango/* src/cairoLib/* \
+	src/glade/* src/gobject/* src/gthread/* src/atk/* \
+	src/pango/* src/cairo/* src/gdkpixbuf/* \
 	srcgl/glgdk/*  srcgl/glgtk/*  srcgl/gtkglc/* -op
 
 
