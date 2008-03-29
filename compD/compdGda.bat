c523374ee6768b16254dff735b8c94751c69d6f1
okibi <okibi@853fb775-85e9-0310-89d4-f5ae88729b16>
2008-03-29 01:14:26 +0000

    updating to the new, second build of pre-release 2 part 2/2
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@485 853fb775-85e9-0310-89d4-f5ae88729b16

---
+++ compD/compdGda.bat
@@ -0,0 +1,1 @@
+compd 	srcgda/gdac 	srcgda/gda 	-I srcgda 	-I src 	-olib libgda 	-l gtkd 	-l dl 	-l stdc++ 	-l phobos 	-g
---
+++ compD/compdGtkDSV.bat
@@ -0,0 +1,3 @@
+del gtkdsv.lib
+compd 	srcsv/gsv 	srcsv/gsvc 	-I src;srcsv;~/dmd/src/phobos 	-olib gtkdsv
+PAUSE
