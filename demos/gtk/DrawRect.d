11ff07e1f452c2e43daefb8599bce40c67c05a08
Mike Wey <Mike Wey@853fb775-85e9-0310-89d4-f5ae88729b16>
2008-07-11 19:21:05 +0000

    replace the gtk/DrawRect demo with the one written by Unarmed - see ticket #12
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@527 853fb775-85e9-0310-89d4-f5ae88729b16

--- demos/gtk/DrawRect.d
+++ demos/gtk/DrawRect.d
@@ -1,13 +1,47 @@
-module gtk.DrawRect;
+module DraRect;
 
-import gdk.Window;
-import glib.ListG;
-import glib.ListSG;
-import glib.Source;
+import gtk.Main;
+import gtk.MainWindow;
+import gtk.Widget;
+import gdk.GC;
+import gdk.Drawable;
+import gdk.Color;
 
-
-void main(char[][] args)
+void main (string[] args)
 {
-	(new Window(null)).drawRectangle(null, false, 0,0,0,0);
+	Main.init(args);
+	MainWindow window = new MainWindow("Gtkd draw rect example");
+	
+	window.addOnHide(
+		delegate void (in Widget whatever)
+		{
+			Main.exit(0);
+		}
+	);
+
+	window.addOnExpose(
+		delegate int (GdkEventExpose * whatever_II, in Widget widget)
+		{
+			Drawable da = widget.getDrawable();
+			da.drawRectangle(	gcFgColor(da, 0, 255, 255), true,
+								0, 0, window.getHeight(), window.getWidth()
+			);
+			return 1;
+		}
+	);
+
+	window.showAll();
+	Main.run();
 }
 
+GC gcFgColor (in Drawable da, int r, int g, int b)
+{
+	if (r<0) r=0;	if (r>255) r=255;
+	if (g<0) r=0;	if (g>255) g=255;
+	if (b<0) r=0;	if (b>255) b=255;
+
+	GC rt = new GC(da);
+	rt.setRgbFgColor( new Color(cast(ubyte)r, cast(ubyte)g, cast(ubyte)b) );
+
+	return rt;
+}
