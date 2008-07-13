ad3104768e07949bce9d37bce05a1139e97bfce5
Mike Wey <Mike Wey@853fb775-85e9-0310-89d4-f5ae88729b16>
2008-07-13 20:36:07 +0000

    Add a demo for popup menu's
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@532 853fb775-85e9-0310-89d4-f5ae88729b16

---
+++ demos/gtk/PopupMenu.d
@@ -0,0 +1,59 @@
+module gtk.PopupMenu;
+
+import gtk.Main;
+import gtk.EventBox;
+import gtk.MainWindow;
+import gtk.Menu;
+import gtk.Label;
+import gtk.ImageMenuItem;
+import gtk.Widget;
+import gtk.AccelGroup;
+
+import tango.io.Stdout;
+
+class ExampleWindow : MainWindow
+{
+	Menu menu;
+
+	this()
+	{
+		super("GtkD: Popup Menu");
+		setDefaultSize(200, 200);
+
+		auto eventBox = new EventBox();
+		eventBox.add( new Label("Right click") );
+		eventBox.addOnButtonPress(&onButtonPress);
+		add(eventBox);
+
+		menu = new Menu();
+		menu.append( new ImageMenuItem(StockID.CUT, cast(AccelGroup)null) );
+		menu.append( new ImageMenuItem(StockID.COPY, cast(AccelGroup)null) );
+		menu.append( new ImageMenuItem(StockID.PASTE, cast(AccelGroup)null) );
+		menu.append( new ImageMenuItem(StockID.DELETE, cast(AccelGroup)null) );
+		menu.attachToWidget(eventBox, null);
+
+		showAll();
+	}
+
+	public int onButtonPress(GdkEventButton* event, Widget widget)
+	{
+		if(event.type == GdkEventType.BUTTON_PRESS && event.button == 3)
+		{
+			Stdout("test").newline;
+
+			menu.showAll();
+			menu.popup(event.button, event.time);
+		}
+
+		return 1;
+	}
+}
+
+void main(string[] arg)
+{
+	Main.init(arg);
+
+	new ExampleWindow();
+
+	Main.run();
+}
