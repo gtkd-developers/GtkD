3c9293b22dd45edb0c2e7784c9eaa7d10fed9ced
Mike Wey <Mike Wey@853fb775-85e9-0310-89d4-f5ae88729b16>
2009-05-30 14:04:35 +0000

    add gio to makedocs.sh
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@684 853fb775-85e9-0310-89d4-f5ae88729b16

--- makedocs.sh
+++ makedocs.sh
@@ -3,7 +3,7 @@ echo "MODULES =" > modules.ddoc ;grep -h -e "^module" src/* -r | sort -u | sed '
 grep -h -e "^module" srcgl/* -r | sort -u | sed 's/;//' | sed 's/\r//' |  sed 's/module \(.*\)$/\t$(MODULE \1)/' >> modules.ddoc
 
 dmd -o- -D -Dddocs  modules.ddoc docs/candy.ddoc \
-	src/gtk/*  src/gtkc/* src/glib/* src/gdk/* \
+	src/gtk/*  src/gtkc/* src/glib/* src/gio/* src/gdk/* \
 	src/glade/* src/gobject/* src/gthread/* src/atk/* \
 	src/pango/* src/cairo/* src/gdkpixbuf/* \
 	srcgl/glgdk/*  srcgl/glgtk/*  srcgl/gtkglc/* \
