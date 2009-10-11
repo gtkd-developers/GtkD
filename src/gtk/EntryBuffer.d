0a2942675b0da7eb08c45bb5b4df92faf56b19d7
Mike Wey <Mike Wey@853fb775-85e9-0310-89d4-f5ae88729b16>
2009-10-11 13:14:24 +0000

    Don't forget the new files ;)
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@714 853fb775-85e9-0310-89d4-f5ae88729b16

---
+++ src/gtk/EntryBuffer.d
@@ -0,0 +1,339 @@
+/*
+ * This file is part of gtkD.
+ *
+ * gtkD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * gtkD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with gtkD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+// generated automatically - do not change
+// find conversion definition on APILookup.txt
+// implement new conversion functionalities on the wrap.utils pakage
+
+/*
+ * Conversion parameters:
+ * inFile  = GtkEntryBuffer.html
+ * outPack = gtk
+ * outFile = EntryBuffer
+ * strct   = GtkEntryBuffer
+ * realStrct=
+ * ctorStrct=
+ * clss    = EntryBuffer
+ * interf  = 
+ * class Code: No
+ * interface Code: No
+ * template for:
+ * extend  = 
+ * implements:
+ * prefixes:
+ * 	- gtk_entry_buffer_
+ * omit structs:
+ * omit prefixes:
+ * omit code:
+ * omit signals:
+ * imports:
+ * 	- glib.Str
+ * structWrap:
+ * module aliases:
+ * local aliases:
+ * overrides:
+ */
+
+module gtk.EntryBuffer;
+
+public  import gtkc.gtktypes;
+
+private import gtkc.gtk;
+private import glib.ConstructionException;
+
+private import gobject.Signals;
+public  import gtkc.gdktypes;
+
+private import glib.Str;
+
+
+
+private import gobject.ObjectG;
+
+/**
+ * Description
+ * The GtkEntryBuffer class contains the actual text displayed in a
+ * GtkEntry widget.
+ * A single GtkEntryBuffer object can be shared by multiple GtkEntry
+ * widgets which will then share the same text content, but not the cursor
+ * position, visibility attributes, icon etc.
+ * GtkEntryBuffer may be derived from. Such a derived class might allow
+ * text to be stored in an alternate location, such as non-pageable memory,
+ * useful in the case of important passwords. Or a derived class could
+ * integrate with an application's concept of undo/redo.
+ */
+public class EntryBuffer : ObjectG
+{
+	
+	/** the main Gtk struct */
+	protected GtkEntryBuffer* gtkEntryBuffer;
+	
+	
+	public GtkEntryBuffer* getEntryBufferStruct()
+	{
+		return gtkEntryBuffer;
+	}
+	
+	
+	/** the main Gtk struct as a void* */
+	protected override void* getStruct()
+	{
+		return cast(void*)gtkEntryBuffer;
+	}
+	
+	/**
+	 * Sets our main struct and passes it to the parent class
+	 */
+	public this (GtkEntryBuffer* gtkEntryBuffer)
+	{
+		if(gtkEntryBuffer is null)
+		{
+			this = null;
+			return;
+		}
+		//Check if there already is a D object for this gtk struct
+		void* ptr = getDObject(cast(GObject*)gtkEntryBuffer);
+		if( ptr !is null )
+		{
+			this = cast(EntryBuffer)ptr;
+			return;
+		}
+		super(cast(GObject*)gtkEntryBuffer);
+		this.gtkEntryBuffer = gtkEntryBuffer;
+	}
+	
+	/**
+	 */
+	int[char[]] connectedSignals;
+	
+	void delegate(guint, guint, EntryBuffer)[] onDeletedTextListeners;
+	/**
+	 */
+	void addOnDeletedText(void delegate(guint, guint, EntryBuffer) dlg, ConnectFlags connectFlags=cast(ConnectFlags)0)
+	{
+		if ( !("deleted-text" in connectedSignals) )
+		{
+			Signals.connectData(
+			getStruct(),
+			"deleted-text",
+			cast(GCallback)&callBackDeletedText,
+			cast(void*)this,
+			null,
+			connectFlags);
+			connectedSignals["deleted-text"] = 1;
+		}
+		onDeletedTextListeners ~= dlg;
+	}
+	extern(C) static void callBackDeletedText(GtkEntryBuffer* entrybufferStruct, guint arg1, guint arg2, EntryBuffer entryBuffer)
+	{
+		foreach ( void delegate(guint, guint, EntryBuffer) dlg ; entryBuffer.onDeletedTextListeners )
+		{
+			dlg(arg1, arg2, entryBuffer);
+		}
+	}
+	
+	void delegate(guint, string, guint, EntryBuffer)[] onInsertedTextListeners;
+	/**
+	 */
+	void addOnInsertedText(void delegate(guint, string, guint, EntryBuffer) dlg, ConnectFlags connectFlags=cast(ConnectFlags)0)
+	{
+		if ( !("inserted-text" in connectedSignals) )
+		{
+			Signals.connectData(
+			getStruct(),
+			"inserted-text",
+			cast(GCallback)&callBackInsertedText,
+			cast(void*)this,
+			null,
+			connectFlags);
+			connectedSignals["inserted-text"] = 1;
+		}
+		onInsertedTextListeners ~= dlg;
+	}
+	extern(C) static void callBackInsertedText(GtkEntryBuffer* entrybufferStruct, guint arg1, gchar* arg2, guint arg3, EntryBuffer entryBuffer)
+	{
+		foreach ( void delegate(guint, string, guint, EntryBuffer) dlg ; entryBuffer.onInsertedTextListeners )
+		{
+			dlg(arg1, Str.toString(arg2), arg3, entryBuffer);
+		}
+	}
+	
+	
+	/**
+	 * Create a new GtkEntryBuffer object.
+	 * Optionally, specify initial text to set in the buffer.
+	 * Since 2.18
+	 * Params:
+	 * initialChars =  initial buffer text, or NULL
+	 * Throws: ConstructionException GTK+ fails to create the object.
+	 */
+	public this (char[] initialChars)
+	{
+		// GtkEntryBuffer* gtk_entry_buffer_new (const gchar *initial_chars,  gint n_initial_chars);
+		auto p = gtk_entry_buffer_new(initialChars.ptr, initialChars.length);
+		if(p is null)
+		{
+			throw new ConstructionException("null returned by gtk_entry_buffer_new(initialChars.ptr, initialChars.length)");
+		}
+		this(cast(GtkEntryBuffer*) p);
+	}
+	
+	/**
+	 * Retrieves the contents of the buffer.
+	 * The memory pointer returned by this call will not change
+	 * unless this object emits a signal, or is finalized.
+	 * Since 2.18
+	 * Returns: a pointer to the contents of the widget as a string. This string points to internally allocated storage in the buffer and must not be freed, modified or stored.
+	 */
+	public string getText()
+	{
+		// const gchar* gtk_entry_buffer_get_text (GtkEntryBuffer *buffer);
+		return Str.toString(gtk_entry_buffer_get_text(gtkEntryBuffer));
+	}
+	
+	/**
+	 * Sets the text in the buffer.
+	 * This is roughly equivalent to calling gtk_entry_buffer_delete_text()
+	 * and gtk_entry_buffer_insert_text().
+	 * Note that n_chars is in characters, not in bytes.
+	 * Since 2.18
+	 * Params:
+	 * chars =  the new text
+	 */
+	public void setText(char[] chars)
+	{
+		// void gtk_entry_buffer_set_text (GtkEntryBuffer *buffer,  const gchar *chars,  gint n_chars);
+		gtk_entry_buffer_set_text(gtkEntryBuffer, chars.ptr, chars.length);
+	}
+	
+	/**
+	 * Retrieves the length in bytes of the buffer.
+	 * See gtk_entry_buffer_get_length().
+	 * Since 2.18
+	 * Returns: The byte length of the buffer.
+	 */
+	public uint getBytes()
+	{
+		// gsize gtk_entry_buffer_get_bytes (GtkEntryBuffer *buffer);
+		return gtk_entry_buffer_get_bytes(gtkEntryBuffer);
+	}
+	
+	/**
+	 * Retrieves the length in characters of the buffer.
+	 * Since 2.18
+	 * Returns: The number of characters in the buffer.
+	 */
+	public uint getLength()
+	{
+		// guint gtk_entry_buffer_get_length (GtkEntryBuffer *buffer);
+		return gtk_entry_buffer_get_length(gtkEntryBuffer);
+	}
+	
+	/**
+	 * Retrieves the maximum allowed length of the text in
+	 * buffer. See gtk_entry_buffer_set_max_length().
+	 * Since 2.18
+	 * Returns: the maximum allowed number of characters in GtkEntryBuffer, or 0 if there is no maximum.
+	 */
+	public int getMaxLength()
+	{
+		// gint gtk_entry_buffer_get_max_length (GtkEntryBuffer *buffer);
+		return gtk_entry_buffer_get_max_length(gtkEntryBuffer);
+	}
+	
+	/**
+	 * Sets the maximum allowed length of the contents of the buffer. If
+	 * the current contents are longer than the given length, then they
+	 * will be truncated to fit.
+	 * Since 2.18
+	 * Params:
+	 * maxLength =  the maximum length of the entry buffer, or 0 for no maximum.
+	 *  (other than the maximum length of entries.) The value passed in will
+	 *  be clamped to the range 0-65536.
+	 */
+	public void setMaxLength(int maxLength)
+	{
+		// void gtk_entry_buffer_set_max_length (GtkEntryBuffer *buffer,  gint max_length);
+		gtk_entry_buffer_set_max_length(gtkEntryBuffer, maxLength);
+	}
+	
+	/**
+	 * Inserts n_chars characters of chars into the contents of the
+	 * buffer, at position position.
+	 * If n_chars is negative, then characters from chars will be inserted
+	 * until a null-terminator is found. If position or n_chars are out of
+	 * bounds, or the maximum buffer text length is exceeded, then they are
+	 * coerced to sane values.
+	 * Note that the position and length are in characters, not in bytes.
+	 * Since 2.18
+	 * Params:
+	 * position =  the position at which to insert text.
+	 * chars =  the text to insert into the buffer.
+	 * Returns: The number of characters actually inserted.
+	 */
+	public uint insertText(uint position, char[] chars)
+	{
+		// guint gtk_entry_buffer_insert_text (GtkEntryBuffer *buffer,  guint position,  const gchar *chars,  gint n_chars);
+		return gtk_entry_buffer_insert_text(gtkEntryBuffer, position, chars.ptr, chars.length);
+	}
+	
+	/**
+	 * Deletes a sequence of characters from the buffer. n_chars characters are
+	 * deleted starting at position. If n_chars is negative, then all characters
+	 * until the end of the text are deleted.
+	 * If position or n_chars are out of bounds, then they are coerced to sane
+	 * values.
+	 * Note that the positions are specified in characters, not bytes.
+	 * Since 2.18
+	 * Params:
+	 * position =  position at which to delete text
+	 * nChars =  number of characters to delete
+	 * Returns: The number of characters deleted.
+	 */
+	public uint deleteText(uint position, int nChars)
+	{
+		// guint gtk_entry_buffer_delete_text (GtkEntryBuffer *buffer,  guint position,  gint n_chars);
+		return gtk_entry_buffer_delete_text(gtkEntryBuffer, position, nChars);
+	}
+	
+	/**
+	 * Used when subclassing GtkEntryBuffer
+	 * Since 2.18
+	 * Params:
+	 * position =  position at which text was deleted
+	 * nChars =  number of characters deleted
+	 */
+	public void emitDeletedText(uint position, uint nChars)
+	{
+		// void gtk_entry_buffer_emit_deleted_text (GtkEntryBuffer *buffer,  guint position,  guint n_chars);
+		gtk_entry_buffer_emit_deleted_text(gtkEntryBuffer, position, nChars);
+	}
+	
+	/**
+	 * Used when subclassing GtkEntryBuffer
+	 * Since 2.18
+	 * Params:
+	 * position =  position at which text was inserted
+	 * chars =  text that was inserted
+	 */
+	public void emitInsertedText(uint position, char[] chars)
+	{
+		// void gtk_entry_buffer_emit_inserted_text (GtkEntryBuffer *buffer,  guint position,  const gchar *chars,  guint n_chars);
+		gtk_entry_buffer_emit_inserted_text(gtkEntryBuffer, position, chars.ptr, chars.length);
+	}
+}
---
+++ src/gtk/InfoBar.d
@@ -0,0 +1,433 @@
+/*
+ * This file is part of gtkD.
+ *
+ * gtkD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * gtkD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with gtkD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+// generated automatically - do not change
+// find conversion definition on APILookup.txt
+// implement new conversion functionalities on the wrap.utils pakage
+
+/*
+ * Conversion parameters:
+ * inFile  = GtkInfoBar.html
+ * outPack = gtk
+ * outFile = InfoBar
+ * strct   = GtkInfoBar
+ * realStrct=
+ * ctorStrct=GtkWidget
+ * clss    = InfoBar
+ * interf  = 
+ * class Code: Yes
+ * interface Code: No
+ * template for:
+ * extend  = 
+ * implements:
+ * prefixes:
+ * 	- gtk_info_bar_
+ * omit structs:
+ * omit prefixes:
+ * omit code:
+ * 	- gtk_info_bar_get_action_area
+ * 	- gtk_info_bar_get_content_area
+ * omit signals:
+ * imports:
+ * 	- glib.Str
+ * 	- gtk.Widget
+ * 	- gtk.HBox;
+ * 	- gtk.VButtonBox
+ * structWrap:
+ * 	- GtkWidget* -> Widget
+ * module aliases:
+ * local aliases:
+ * overrides:
+ */
+
+module gtk.InfoBar;
+
+public  import gtkc.gtktypes;
+
+private import gtkc.gtk;
+private import glib.ConstructionException;
+
+private import gobject.Signals;
+public  import gtkc.gdktypes;
+
+private import glib.Str;
+private import gtk.Widget;
+private import gtk.HBox;;
+private import gtk.VButtonBox;
+
+
+
+private import gtk.HBox;
+
+/**
+ * Description
+ * GtkInfoBar is a widget that can be used to show messages to
+ * the user without showing a dialog. It is often temporarily shown
+ * at the top or bottom of a document. In contrast to GtkDialog, which
+ * has a horizontal action area at the bottom, GtkInfoBar has a
+ * vertical action area at the side.
+ * The API of GtkInfoBar is very similar to GtkDialog, allowing you
+ * to add buttons to the action area with gtk_info_bar_add_button() or
+ * gtk_info_bar_new_with_buttons(). The sensitivity of action widgets
+ * can be controlled with gtk_info_bar_set_response_sensitive().
+ * To add widgets to the main content area of a GtkInfoBar, use
+ * gtk_info_bar_get_content_area() and add your widgets to the container.
+ * Similar to GtkMessageDialog, the contents of a GtkInfoBar can by
+ * classified as error message, warning, informational message, etc,
+ * by using gtk_info_bar_set_message_type(). GTK+ uses the message type
+ * to determine the background color of the message area.
+ * Example 14. Simple GtkInfoBar usage.
+ * /+* set up info bar +/
+ * info_bar = gtk_info_bar_new ();
+ * gtk_widget_set_no_show_all (info_bar, TRUE);
+ * message_label = gtk_label_new ("");
+ * gtk_widget_show (message_label);
+ * content_area = gtk_info_bar_get_content_area (GTK_INFO_BAR (info_bar));
+ * gtk_container_add (GTK_CONTAINER (content_area), message_label);
+ * gtk_info_bar_add_button (GTK_INFO_BAR (info_bar),
+ *  GTK_STOCK_OK, GTK_RESPONSE_OK);
+ * g_signal_connect (info_bar, "response",
+ *  G_CALLBACK (gtk_widget_hide), NULL);
+ * gtk_table_attach (GTK_TABLE (table),
+ *  info_bar,
+ *  0, 1, 2, 3,
+ *  GTK_EXPAND | GTK_FILL, 0,
+ *  0, 0);
+ * /+* ... +/
+ * /+* show an error message +/
+ * gtk_label_set_text (GTK_LABEL (message_label), error_message);
+ * gtk_info_bar_set_message_type (GTK_INFO_BAR (info_bar),
+ *  GTK_MESSAGE_ERROR);
+ * gtk_widget_show (info_bar);
+ * GtkInfoBar as GtkBuildable
+ * The GtkInfoBar implementation of the GtkBuildable interface exposes
+ * the content area and action area as internal children with the names
+ * "content_area" and "action_area".
+ * GtkInfoBar supports a custom <action-widgets> element, which
+ * can contain multiple <action-widget> elements. The "response"
+ * attribute specifies a numeric response, and the content of the element
+ * is the id of widget (which should be a child of the dialogs action_area).
+ */
+public class InfoBar : HBox
+{
+	
+	/** the main Gtk struct */
+	protected GtkInfoBar* gtkInfoBar;
+	
+	
+	public GtkInfoBar* getInfoBarStruct()
+	{
+		return gtkInfoBar;
+	}
+	
+	
+	/** the main Gtk struct as a void* */
+	protected override void* getStruct()
+	{
+		return cast(void*)gtkInfoBar;
+	}
+	
+	/**
+	 * Sets our main struct and passes it to the parent class
+	 */
+	public this (GtkInfoBar* gtkInfoBar)
+	{
+		if(gtkInfoBar is null)
+		{
+			this = null;
+			return;
+		}
+		//Check if there already is a D object for this gtk struct
+		void* ptr = getDObject(cast(GObject*)gtkInfoBar);
+		if( ptr !is null )
+		{
+			this = cast(InfoBar)ptr;
+			return;
+		}
+		super(cast(GtkHBox*)gtkInfoBar);
+		this.gtkInfoBar = gtkInfoBar;
+	}
+	
+	/** */
+	public this(string[] buttonsText, ResponseType[] responses)
+	{
+		this();
+		
+		for ( int i=0 ; i<buttonsText.length && i<responses.length ; i++)
+		{
+			addButton(buttonsText[i], responses[i]);
+		}
+	}
+	
+	/** */
+	public this(StockID[] stockIDs, ResponseType[] responses)
+	{
+		this();
+		
+		for ( int i=0 ; i<stockIDs.length && i<responses.length ; i++)
+		{
+			addButton(stockIDs[i], responses[i]);
+		}
+	}
+	
+	/** */
+	public Widget addButton(StockID stockID, int responseId)
+	{
+		return addButton(StockDesc[stockID], responseId);
+	}
+	
+	/** */
+	public void addButtons(string[] buttonsText, ResponseType[] responses)
+	{
+		for ( int i=0 ; i<buttonsText.length && i<responses.length ; i++)
+		{
+			addButton(buttonsText[i], responses[i]);
+		}
+	}
+	
+	/** */
+	public void addButtons(StockID[] stockIDs, ResponseType[] responses)
+	{
+		for ( int i=0 ; i<stockIDs.length && i<responses.length ; i++)
+		{
+			addButton(stockIDs[i], responses[i]);
+		}
+	}
+	
+	/**
+	 * Returns the action area of info_bar.
+	 * Since 2.18
+	 * Returns: the action area.
+	 */
+	public VButtonBox getActionArea()
+	{
+		// GtkWidget * gtk_info_bar_get_action_area (GtkInfoBar *info_bar);
+		auto p = gtk_info_bar_get_action_area(gtkInfoBar);
+		if(p is null)
+		{
+			return null;
+		}
+		return new VButtonBox(cast(GtkVButtonBox*) p);
+	}
+	
+	/**
+	 * Returns the content area of info_bar.
+	 * Since 2.18
+	 * Returns: the content area.
+	 */
+	public HBox getContentArea()
+	{
+		// GtkWidget * gtk_info_bar_get_content_area (GtkInfoBar *info_bar);
+		auto p = gtk_info_bar_get_content_area(gtkInfoBar);
+		if(p is null)
+		{
+			return null;
+		}
+		return new HBox(cast(GtkHBox*) p);
+	}
+	
+	/**
+	 */
+	int[char[]] connectedSignals;
+	
+	void delegate(InfoBar)[] onCloseListeners;
+	/**
+	 * The ::close signal is a
+	 * keybinding signal
+	 * which gets emitted when the user uses a keybinding to dismiss
+	 * the info bar.
+	 * The default binding for this signal is the Escape key.
+	 * Since 2.18
+	 */
+	void addOnClose(void delegate(InfoBar) dlg, ConnectFlags connectFlags=cast(ConnectFlags)0)
+	{
+		if ( !("close" in connectedSignals) )
+		{
+			Signals.connectData(
+			getStruct(),
+			"close",
+			cast(GCallback)&callBackClose,
+			cast(void*)this,
+			null,
+			connectFlags);
+			connectedSignals["close"] = 1;
+		}
+		onCloseListeners ~= dlg;
+	}
+	extern(C) static void callBackClose(GtkInfoBar* arg0Struct, InfoBar infoBar)
+	{
+		foreach ( void delegate(InfoBar) dlg ; infoBar.onCloseListeners )
+		{
+			dlg(infoBar);
+		}
+	}
+	
+	void delegate(gint, InfoBar)[] onResponseListeners;
+	/**
+	 * Emitted when an action widget is clicked or the application programmer
+	 * calls gtk_dialog_response(). The response_id depends on which action
+	 * widget was clicked.
+	 * Since 2.18
+	 * See Also
+	 * #GtkStatusbar, GtkMessageDialog
+	 */
+	void addOnResponse(void delegate(gint, InfoBar) dlg, ConnectFlags connectFlags=cast(ConnectFlags)0)
+	{
+		if ( !("response" in connectedSignals) )
+		{
+			Signals.connectData(
+			getStruct(),
+			"response",
+			cast(GCallback)&callBackResponse,
+			cast(void*)this,
+			null,
+			connectFlags);
+			connectedSignals["response"] = 1;
+		}
+		onResponseListeners ~= dlg;
+	}
+	extern(C) static void callBackResponse(GtkInfoBar* infoBarStruct, gint responseId, InfoBar infoBar)
+	{
+		foreach ( void delegate(gint, InfoBar) dlg ; infoBar.onResponseListeners )
+		{
+			dlg(responseId, infoBar);
+		}
+	}
+	
+	
+	/**
+	 * Creates a new GtkInfoBar object.
+	 * Since 2.18
+	 * Throws: ConstructionException GTK+ fails to create the object.
+	 */
+	public this ()
+	{
+		// GtkWidget * gtk_info_bar_new (void);
+		auto p = gtk_info_bar_new();
+		if(p is null)
+		{
+			throw new ConstructionException("null returned by gtk_info_bar_new()");
+		}
+		this(cast(GtkInfoBar*) p);
+	}
+	
+	/**
+	 * Add an activatable widget to the action area of a GtkInfoBar,
+	 * connecting a signal handler that will emit the "response"
+	 * signal on the message area when the widget is activated. The widget
+	 * is appended to the end of the message areas action area.
+	 * Since 2.18
+	 * Params:
+	 * child =  an activatable widget
+	 * responseId =  response ID for child
+	 */
+	public void addActionWidget(Widget child, int responseId)
+	{
+		// void gtk_info_bar_add_action_widget (GtkInfoBar *info_bar,  GtkWidget *child,  gint response_id);
+		gtk_info_bar_add_action_widget(gtkInfoBar, (child is null) ? null : child.getWidgetStruct(), responseId);
+	}
+	
+	/**
+	 * Adds a button with the given text (or a stock button, if button_text
+	 * is a stock ID) and sets things up so that clicking the button will emit
+	 * the "response" signal with the given response_id. The button is appended
+	 * to the end of the info bars's action area. The button widget is
+	 * returned, but usually you don't need it.
+	 * Since 2.18
+	 * Params:
+	 * buttonText =  text of button, or stock ID
+	 * responseId =  response ID for the button
+	 * Returns: the button widget that was added
+	 */
+	public Widget addButton(string buttonText, int responseId)
+	{
+		// GtkWidget * gtk_info_bar_add_button (GtkInfoBar *info_bar,  const gchar *button_text,  gint response_id);
+		auto p = gtk_info_bar_add_button(gtkInfoBar, Str.toStringz(buttonText), responseId);
+		if(p is null)
+		{
+			return null;
+		}
+		return new Widget(cast(GtkWidget*) p);
+	}
+	
+	/**
+	 * Calls gtk_widget_set_sensitive (widget, setting) for each
+	 * widget in the info bars's action area with the given response_id.
+	 * A convenient way to sensitize/desensitize dialog buttons.
+	 * Since 2.18
+	 * Params:
+	 * responseId =  a response ID
+	 * setting =  TRUE for sensitive
+	 */
+	public void setResponseSensitive(int responseId, int setting)
+	{
+		// void gtk_info_bar_set_response_sensitive (GtkInfoBar *info_bar,  gint response_id,  gboolean setting);
+		gtk_info_bar_set_response_sensitive(gtkInfoBar, responseId, setting);
+	}
+	
+	/**
+	 * Sets the last widget in the info bar's action area with
+	 * the given response_id as the default widget for the dialog.
+	 * Pressing "Enter" normally activates the default widget.
+	 * Since 2.18
+	 * Params:
+	 * responseId =  a response ID
+	 */
+	public void setDefaultResponse(int responseId)
+	{
+		// void gtk_info_bar_set_default_response (GtkInfoBar *info_bar,  gint response_id);
+		gtk_info_bar_set_default_response(gtkInfoBar, responseId);
+	}
+	
+	/**
+	 * Emits the 'response' signal with the given response_id.
+	 * Since 2.18
+	 * Params:
+	 * responseId =  a response ID
+	 */
+	public void response(int responseId)
+	{
+		// void gtk_info_bar_response (GtkInfoBar *info_bar,  gint response_id);
+		gtk_info_bar_response(gtkInfoBar, responseId);
+	}
+	
+	/**
+	 * Sets the message type of the message area.
+	 * GTK+ uses this type to determine what color to use
+	 * when drawing the message area.
+	 * Since 2.18
+	 * Params:
+	 * messageType =  a GtkMessageType
+	 */
+	public void setMessageType(GtkMessageType messageType)
+	{
+		// void gtk_info_bar_set_message_type (GtkInfoBar *info_bar,  GtkMessageType message_type);
+		gtk_info_bar_set_message_type(gtkInfoBar, messageType);
+	}
+	
+	/**
+	 * Returns the message type of the message area.
+	 * Since 2.18
+	 * Returns: the message type of the message area.
+	 */
+	public GtkMessageType getMessageType()
+	{
+		// GtkMessageType gtk_info_bar_get_message_type (GtkInfoBar *info_bar);
+		return gtk_info_bar_get_message_type(gtkInfoBar);
+	}
+}
