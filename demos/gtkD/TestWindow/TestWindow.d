5faff97cb3ae125123e7e9eaf31e851e15b984d0
Mike Wey <Mike Wey@853fb775-85e9-0310-89d4-f5ae88729b16>
2011-03-13 19:08:42 +0000

    comitted the wrong file for the demo
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@809 853fb775-85e9-0310-89d4-f5ae88729b16

--- demos/gtkD/TestWindow/TestWindow.d
+++ demos/gtkD/TestWindow/TestWindow.d
@@ -811,24 +811,20 @@ class TestWindow : MainWindow
 	void showFileSel(Button button)
 	{
 
-		Dialog dlg = new Dialog("title", this, cast(GtkDialogFlags)0, [StockID.OK], [GtkResponseType.GTK_RESPONSE_CLOSE]);
-
-
-		dlg.run();
-//		if ( fs  is  null )
-//		{
-//			fs = new FileSelection("File Selection");
-//		}
+		if ( fs  is  null )
+		{
+			fs = new FileSelection("File Selection");
+		}
 		//fs.showAll();
 		//fs.setMultiple(true);
-//		fs.run();
+		fs.run();
 		//printf("file selected = %.*s\n",fs.getFileName());
 //		string[] selections = fs.getSelections();
 //		for ( int i=0 ;i<selections.length ; i++)
 //		{
 //			printf("File(s) selected [%d] %.*s\n",i,selections[i]);
 //		}
-//		fs.hide();
+		fs.hide();
 	}
 
 	void showColor(Button button)
