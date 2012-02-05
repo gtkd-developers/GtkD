6606529688639e53012442000ef781497b1c5f55
Mike Wey <Mike Wey@853fb775-85e9-0310-89d4-f5ae88729b16>
2012-02-05 16:08:23 +0000

    Make sure the license in the source files is in sync with the one in COPYING. - Closes ticket #113
    git-svn-id: http://svn.dsource.org/projects/gtkd/trunk@932 853fb775-85e9-0310-89d4-f5ae88729b16

--- demos/gtkD/TestWindow/TestWindow.d
+++ demos/gtkD/TestWindow/TestWindow.d
@@ -3,7 +3,7 @@
  *
  * gtkD is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * gtkD is distributed in the hope that it will be useful,
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with gtkD; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
  */
 
 module TestWindow;
--- wrap/utils/GtkWrapper.d
+++ wrap/utils/GtkWrapper.d
@@ -3,7 +3,7 @@
  *
  * gtkD is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * gtkD is distributed in the hope that it will be useful,
@@ -107,7 +107,7 @@ public class GtkWrapper : WrapperIF
 "\n * "
 "\n * gtkD is free software; you can redistribute it and/or modify"
 "\n * it under the terms of the GNU Lesser General Public License as published by"
-"\n * the Free Software Foundation; either version 2.1 of the License, or"
+"\n * the Free Software Foundation; either version 3 of the License, or"
 "\n * (at your option) any later version."
 "\n * "
 "\n * gtkD is distributed in the hope that it will be useful,"
