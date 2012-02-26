494533a558c2d7b7574848e1f7e9c201cc266691
Mike Wey <mike@mikewey.eu>
2012-02-26 17:40:33 +0100

    gdk.GdkCairo should be empty.

--- src/gdk/GdkCairo.d
+++ src/gdk/GdkCairo.d
@@ -49,7 +49,7 @@
  * 	- gdk_cairo_rectangle
  * 	- gdk_cairo_region
  * 	- gdk_cairo_reset_clip
- * 	- void gdk_cairo_set_source_window
+ * 	- gdk_cairo_set_source_window
  * omit signals:
  * imports:
  * 	- cairo.Context
@@ -90,24 +90,4 @@ private import cairo.Context;
 
 /**
  */
-
-/**
- * Sets the given window as the source pattern for the Cairo context.
- * The pattern has an extend mode of CAIRO_EXTEND_NONE and is aligned
- * so that the origin of window is x, y. The window contains all its
- * subwindows when rendering.
- * Note that the contents of window are undefined outside of the
- * visible part of window, so use this function with care.
- * Since 2.24
- * Params:
- * cr = a Cairo context
- * window = a GdkWindow
- * x = X coordinate of location to place upper left corner of window
- * y = Y coordinate of location to place upper left corner of window
- */
-public static void setSourceWindow(Context cr, Window window, double x, double y)
-{
-	// void gdk_cairo_set_source_window (cairo_t *cr,  GdkWindow *window,  double x,  double y);
-	gdk_cairo_set_source_window((cr is null) ? null : cr.getContextStruct(), (window is null) ? null : window.getWindowStruct(), x, y);
-}
 
