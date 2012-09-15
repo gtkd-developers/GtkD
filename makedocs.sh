f10c2878ae064e836e61fe3e6f5561ec7e9eb161
Mike Wey <mike@mikewey.eu>
2012-09-15 16:24:01 +0200

    Update makedocs for the missing gtkgl packages.

--- makedocs.sh
+++ makedocs.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 echo "MODULES =" > modules.ddoc ;grep -h -e "^module" src/* -r | sort -u | sed 's/;//' | sed 's/\r//' |  sed 's/module \(.*\)$/\t$(MODULE \1)/' >> modules.ddoc
-grep -h -e "^module" srcgl/* -r | sort -u | sed 's/;//' | sed 's/\r//' |  sed 's/module \(.*\)$/\t$(MODULE \1)/' >> modules.ddoc
+grep -h -e "^module" srcsv/* -r | sort -u | sed 's/;//' | sed 's/\r//' |  sed 's/module \(.*\)$/\t$(MODULE \1)/' >> modules.ddoc
 
 dmd -o- -D -Dddocs  modules.ddoc docs/candy.ddoc \
 	src/gtk/*  src/gtkc/* src/glib/* src/gio/* src/gdk/* \
-	src/glade/* src/gobject/* src/gthread/* src/atk/* \
+	src/gobject/* src/gthread/* src/atk/* \
 	src/pango/* src/cairo/* src/gdkpixbuf/* \
-	srcgl/glgdk/*  srcgl/glgtk/*  srcgl/gtkglc/* \
 	srcgda/gda/*  srcgda/gdac/* \
 	srcsv/gsv/*  srcsv/gsvc/* -op
 
