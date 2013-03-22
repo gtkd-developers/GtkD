3a4a06e5a18e6e7bed5f39700a6726955adafbf3
Mike Wey <mike@mikewey.eu>
2013-03-22 23:10:05 +0100

    Also generate documentation for the GtkDGL packages.

--- makedocs.sh
+++ makedocs.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 echo "MODULES =" > modules.ddoc ;grep -h -e "^module" src/* -r | sort -u | sed 's/;//' | sed 's/\r//' |  sed 's/module \(.*\)$/\t$(MODULE \1)/' >> modules.ddoc
+grep -h -e "^module" srcgl/* -r | sort -u | sed 's/;//' | sed 's/\r//' |  sed 's/module \(.*\)$/\t$(MODULE \1)/' >> modules.ddoc
 grep -h -e "^module" srcsv/* -r | sort -u | sed 's/;//' | sed 's/\r//' |  sed 's/module \(.*\)$/\t$(MODULE \1)/' >> modules.ddoc
 
 dmd -o- -D -Dddocs  modules.ddoc docs/candy.ddoc \
 	src/gtk/*  src/gtkc/* src/glib/* src/gio/* src/gdk/* \
 	src/gobject/* src/gthread/* src/atk/* \
 	src/pango/* src/cairo/* src/gdkpixbuf/* \
-	srcgda/gda/*  srcgda/gdac/* \
+	srcgl/glgdk/*  srcgl/glgtk/*  srcgl/gtkglc/* \
 	srcsv/gsv/*  srcsv/gsvc/* -op
 
 
