5073a70a43f539ce64984ca24fb9b5dadc183d9d
Mike Wey <mike@mikewey.eu>
2013-05-26 19:14:24 +0200

    Check if obj is null in getDObject, Fix #54

--- src/gobject/ObjectG.d
+++ src/gobject/ObjectG.d
@@ -248,6 +248,9 @@ public class ObjectG
 	 */
 	public static T getDObject(T, U)(U obj)
 	{
+		if ( obj is null )
+		return null;
+		
 		static if ( is(T : ObjectG) )
 		{
 			auto p = g_object_get_data(cast(GObject*)obj, Str.toStringz("GObject"));
