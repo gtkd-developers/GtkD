cb0b742ec8996b136fdc29fae7b0aa64f6d1c39c
Mike Wey <mike@mikewey.eu>
2015-02-22 18:44:42 +0100

    Add missing import for PixbufLoader.

--- src/gdkpixbuf/PixbufLoader.d
+++ src/gdkpixbuf/PixbufLoader.d
@@ -31,6 +31,7 @@ private import glib.Bytes;
 private import glib.ConstructionException;
 private import glib.ErrorG;
 private import glib.GException;
+private import glib.Str;
 private import gobject.ObjectG;
 private import gobject.Signals;
 private import gtkc.gdkpixbuf;
